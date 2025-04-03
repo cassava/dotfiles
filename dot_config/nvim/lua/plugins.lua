495aff96ee34c466d1b09e2de076713f2493f9e1
Ben Morgan <cassava@iexu.de>
2025-04-03 15:43:28 +0200

    fix(nvim): Remove xvzc/chezmoi.nvim plugin
    As it turns out, automatically applying files is bad when
    working on scripts.

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -842,24 +842,6 @@ return {
   },
 
 -- Tools
-  { "xvzc/chezmoi.nvim",
-    about = "Automatically apply chezmoi-managed files.",
-    init = function()
-      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-          pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
-          callback = function(ev)
-              local bufnr = ev.buf
-              local edit_watch = function()
-                  require("chezmoi.commands.__edit").watch(bufnr)
-              end
-              vim.schedule(edit_watch)
-          end,
-      })
-    end,
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = true,
-  },
-
   { "qpkorr/vim-renamer",
     about = "Rename files in the system with: nvim +Renamer",
     cmd = "Renamer",
