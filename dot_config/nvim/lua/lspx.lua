0462c2819fe7c2bb732df49e1427bd04d7db8082
Ben Morgan <cassava@iexu.de>
2025-05-14 16:06:01 +0200

    feat(nvim): Replace nvim-cmp with blink.cmp

--- dot_config/nvim/lua/lspx.lua
+++ dot_config/nvim/lua/lspx.lua
@@ -161,108 +161,57 @@ return {
     end,
   },
 
-  { "hrsh7th/nvim-cmp",
-    about = "Provide completion.",
-    event = "InsertEnter",
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup {
-        mapping = cmp.mapping.preset.insert({
-          ['<c-p>'] = cmp.mapping.select_prev_item(),
-          ['<c-n>'] = cmp.mapping.select_next_item(),
-          ['<c-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<c-f>'] = cmp.mapping.scroll_docs(4),
-          ['<c-space>'] = cmp.mapping.complete(),
-          ['<c-e>'] = cmp.mapping.abort(),
-          ['<c-y>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
-        }),
-        formatting = {
-          -- format = require("lspkind").cmp_format({
-          --   mode = "symbol_text",
-          --   menu = ({
-          --     buffer = "[buffer]",
-          --     calc = "[calc]",
-          --     cmdline = "[cmdline]",
-          --     luasnip = "[snip]",
-          --     nvim_lsp = "[lsp]",
-          --     path = "[path]",
-          --   })
-          -- }),
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        sources = {
-          { name = "nvim_lsp",
-            option = {
-              markdown_oxide = {
-                keyword_pattern = [[\(\k\| \|\/\|#\)\+]]
+  { "saghen/blink.cmp",
+    website = "https://cmp.saghen.dev/",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "Kaiser-Yang/blink-cmp-git",
+    },
+    event = "VeryLazy",
+    version = "1.*",
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- signature = { enabled = false },
+      -- completion = {
+      --   documentation = {
+      --     auto_show = false
+      --   },
+      --   ghost_text = { enabled = false },
+      -- },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lazydev", "git", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          git = {
+            name = "Git",
+            module = "blink-cmp-git",
+            opts = {
+              commit = {
+                triggers = { "git:" },
               }
             }
           },
-          { name = "nvim_lsp_signature_help" },
-          { name = "luasnip" },
-          { name = "path" },
-          { name = "calc" },
-          { name = "buffer", keyword_length = 5 },
-        },
-        enabled = function()
-          -- While this function will cause it to be disabled in many cases,
-          -- you can always still get completion by using <c-space>.
-          local in_prompt = vim.api.nvim_buf_get_option(0, "buftype") == "prompt"
-          if in_prompt then
-            -- this will disable cmp in the Telescope window
-            return false
-          end
-          local context = require("cmp.config.context")
-          return not(context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
-        end,
-        experimental = {
-          ghost_text = {
-            hl_group = "LspCodeLens",
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
           },
-        },
-      }
-
-      cmp.setup.filetype("gitcommit", {
-        sources = cmp.config.sources({
-          { name = "cmp_git" },
-        }, {
-            { name = "buffer" },
-        })
-      })
-
-      -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({"/", "?"}, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" }
-        },
-      })
-
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" }
-        }, {
-          { name = "cmdline" }
-        })
-      })
-    end,
-    dependencies = {
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-cmdline" },
-      { "hrsh7th/cmp-buffer" },
-      { "hrsh7th/cmp-path" },
-      { "hrsh7th/cmp-calc" },
-      { "hrsh7th/cmp-nvim-lsp-signature-help" },
-      { "saadparwaiz1/cmp_luasnip" },
+        }
+      },
+    },
+    opts_extend = { "sources.default" },
+    keys = {
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
     }
   },
 
@@ -330,75 +279,6 @@ return {
     opts = {
       commented = true,
     }
-  },
-
-  -- FIXME: Broken since latest update
-  { "L3MON4D3/LuaSnip", -- DISABLED
-    about = "Powerful snippet engine.",
-    enabled = false,
-    event = "InsertEnter",
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
-    },
-    config = function(_, opts)
-      require("luasnip").setup(opts)
-      require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/snippets"})
-      vim.api.nvim_create_user_command("LuaSnipEdit", function() require("luasnip.loaders.from_lua").edit_snippet_files() end, {})
-    end,
-    keys = {
-      {
-        "<c-k>",
-        function()
-          local ls = require("luasnip")
-          if ls.expand_or_jumpable() then
-              ls.expand_or_jump()
-          end
-        end,
-        mode = {"i", "s"},
-        desc = "Expand snippet / Next snippet position"
-      },
-      {
-        "<c-j>",
-        function()
-          local ls = require("luasnip")
-          if ls.jumpable(-1) then
-              ls.jump(-1)
-          end
-        end,
-        mode = {"i", "s"},
-        desc = "Previous snippet position"
-      },
-      {
-        "<c-l>",
-        function()
-          local ls = require("luasnip")
-          if ls.choice_active() then
-              ls.change_choice(1)
-              return "<nop>"
-          else
-            return "<c-l>"
-          end
-        end,
-        expr = true,
-        mode = "i",
-        desc = "Change snippet choice",
-      },
-      {
-        "<leader>vn",
-        "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<cr>",
-        desc = "Reload snippets"
-      },
-    },
-    dependencies = {
-      {
-        "rafamadriz/friendly-snippets",
-        about = "Snippet collection.",
-        config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-      },
-    },
   },
 
 --- TREESITTER ----------------------------------------------------------------
