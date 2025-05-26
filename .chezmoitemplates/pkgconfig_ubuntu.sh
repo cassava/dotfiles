5ad1d80cb8e841564dae4271b6033a1cb9774efa
Ben Morgan <cassava@iexu.de>
2025-05-26 09:22:11 +0200

    fix(chezmoi): Fix shellcheck warnings

--- .chezmoitemplates/pkgconfig_ubuntu.sh
+++ .chezmoitemplates/pkgconfig_ubuntu.sh
@@ -9,8 +9,11 @@
 #     sudo_copy_bin FILE [NAME]
 #     sudo_innstall_deb FILE
 #     sudo_untar_fonts FILE
+#
+# Notes:
+# - It's generally not necessary to provide an install function for .deb files.
 
-declare -a packages=(
+declare -ax packages=(
     age
     autoconf
     automake
@@ -47,37 +50,37 @@ declare -a packages=(
 # {{ end }}
 )
 
-declare -A assets=(
-    ["ast-grep"]={{ gitHubLatestReleaseAssetURL "ast-grep/ast-grep" (printf "app-%s-unknown-linux-gnu.zip" .sys.uname_arch) }}
-    ["bat"]={{ gitHubLatestReleaseAssetURL "sharkdp/bat" (printf "bat_*_%s.deb" .chezmoi.arch) }}
-    ["binocle"]={{ gitHubLatestReleaseAssetURL "sharkdp/binocle" (printf "binocle-musl_*_%s.deb" .chezmoi.arch) }}
-    ["bottom"]={{ gitHubLatestReleaseAssetURL "clementtsang/bottom" (printf "bottom-musl_*_%s.deb" .chezmoi.arch) }}
-    ["buildifier"]={{ gitHubLatestReleaseAssetURL "bazelbuild/buildtools" (printf "buildifier-linux-%s" .chezmoi.arch) }}
-    ["buildozer"]={{ gitHubLatestReleaseAssetURL "bazelbuild/buildtools" (printf "buildozer-linux-%s" .chezmoi.arch) }}
-    ["delta"]={{ gitHubLatestReleaseAssetURL "dandavison/delta" (printf "git-delta-musl_*_%s.deb" .chezmoi.arch) }}
-    ["diskus"]={{ gitHubLatestReleaseAssetURL "sharkdp/diskus" (printf "diskus-musl_*_%s.deb" .chezmoi.arch) }}
-    ["fd"]={{ gitHubLatestReleaseAssetURL "sharkdp/fd" (printf "fd-musl_*_%s.deb" .chezmoi.arch) }}
-    ["fzf"]={{ gitHubLatestReleaseAssetURL "junegunn/fzf" (printf "fzf-*-linux_%s.tar.gz" .chezmoi.arch) }}
-    ["gojq"]={{ gitHubLatestReleaseAssetURL "itchyny/gojq" (printf "gojq_*_linux_%s.tar.gz" .chezmoi.arch) }}
-    ["hexyl"]={{ gitHubLatestReleaseAssetURL "sharkdp/hexyl" (printf "hexyl-musl_*_%s.deb" .chezmoi.arch) }}
-    ["hyperfine"]={{ gitHubLatestReleaseAssetURL "sharkdp/hyperfine" (printf "hyperfine-musl_*_%s.deb" .chezmoi.arch) }}
-    ["lsd"]={{ gitHubLatestReleaseAssetURL "lsd-rs/lsd" (printf "lsd-musl_*_%s.deb" .chezmoi.arch) }}
-    ["numbat"]={{ gitHubLatestReleaseAssetURL "sharkdp/numbat" (printf "numbat-musl_*_%s.deb" .chezmoi.arch) }}
-    ["nvim"]={{ gitHubLatestReleaseAssetURL "neovim/neovim" (printf "nvim-linux-%s.tar.gz" .sys.uname_arch) }}
-    ["ripgrep"]={{ gitHubLatestReleaseAssetURL "burntsushi/ripgrep" (printf "ripgrep_*_%s.deb" .chezmoi.arch) }}
-    ["sd"]={{ gitHubLatestReleaseAssetURL "chmln/sd" (printf "sd-*-%s-unknown-linux-musl.tar.gz" .sys.uname_arch) }}
-    ["serie"]={{ gitHubLatestReleaseAssetURL "lusingander/serie" (printf "serie-*-%s-unknown-linux-musl.tar.gz" .sys.uname_arch) }}
-    ["tectonic"]={{ gitHubLatestReleaseAssetURL "tectonic-typesetting/tectonic" (printf "tectonic-*-%s-unknown-linux-musl.tar.gz" .sys.uname_arch) }}
-    ["unused_deps"]={{ gitHubLatestReleaseAssetURL "bazelbuild/buildtools" (printf "unused_deps-linux-%s" .chezmoi.arch) }}
-    ["uv"]={{ gitHubLatestReleaseAssetURL "astral-sh/uv" (printf "uv-%s-unknown-linux-musl.tar.gz" .sys.uname_arch) }}
-    ["watchexec"]={{ gitHubLatestReleaseAssetURL "watchexec/watchexec" (printf "watchexec-*-%s-unknown-linux-musl.deb" .sys.uname_arch) }}
+declare -Ax assets=(
+    ["ast-grep"]='{{ gitHubLatestReleaseAssetURL "ast-grep/ast-grep" (printf "app-%s-unknown-linux-gnu.zip" .sys.uname_arch) }}'
+    ["bat"]='{{ gitHubLatestReleaseAssetURL "sharkdp/bat" (printf "bat_*_%s.deb" .chezmoi.arch) }}'
+    ["binocle"]='{{ gitHubLatestReleaseAssetURL "sharkdp/binocle" (printf "binocle-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["bottom"]='{{ gitHubLatestReleaseAssetURL "clementtsang/bottom" (printf "bottom-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["buildifier"]='{{ gitHubLatestReleaseAssetURL "bazelbuild/buildtools" (printf "buildifier-linux-%s" .chezmoi.arch) }}'
+    ["buildozer"]='{{ gitHubLatestReleaseAssetURL "bazelbuild/buildtools" (printf "buildozer-linux-%s" .chezmoi.arch) }}'
+    ["delta"]='{{ gitHubLatestReleaseAssetURL "dandavison/delta" (printf "git-delta-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["diskus"]='{{ gitHubLatestReleaseAssetURL "sharkdp/diskus" (printf "diskus-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["fd"]='{{ gitHubLatestReleaseAssetURL "sharkdp/fd" (printf "fd-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["fzf"]='{{ gitHubLatestReleaseAssetURL "junegunn/fzf" (printf "fzf-*-linux_%s.tar.gz" .chezmoi.arch) }}'
+    ["gojq"]='{{ gitHubLatestReleaseAssetURL "itchyny/gojq" (printf "gojq_*_linux_%s.tar.gz" .chezmoi.arch) }}'
+    ["hexyl"]='{{ gitHubLatestReleaseAssetURL "sharkdp/hexyl" (printf "hexyl-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["hyperfine"]='{{ gitHubLatestReleaseAssetURL "sharkdp/hyperfine" (printf "hyperfine-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["lsd"]='{{ gitHubLatestReleaseAssetURL "lsd-rs/lsd" (printf "lsd-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["numbat"]='{{ gitHubLatestReleaseAssetURL "sharkdp/numbat" (printf "numbat-musl_*_%s.deb" .chezmoi.arch) }}'
+    ["nvim"]='{{ gitHubLatestReleaseAssetURL "neovim/neovim" (printf "nvim-linux-%s.tar.gz" .sys.uname_arch) }}'
+    ["ripgrep"]='{{ gitHubLatestReleaseAssetURL "burntsushi/ripgrep" (printf "ripgrep_*_%s.deb" .chezmoi.arch) }}'
+    ["sd"]='{{ gitHubLatestReleaseAssetURL "chmln/sd" (printf "sd-*-%s-unknown-linux-musl.tar.gz" .sys.uname_arch) }}'
+    ["serie"]='{{ gitHubLatestReleaseAssetURL "lusingander/serie" (printf "serie-*-%s-unknown-linux-musl.tar.gz" .sys.uname_arch) }}'
+    ["tectonic"]='{{ gitHubLatestReleaseAssetURL "tectonic-typesetting/tectonic" (printf "tectonic-*-%s-unknown-linux-musl.tar.gz" .sys.uname_arch) }}'
+    ["unused_deps"]='{{ gitHubLatestReleaseAssetURL "bazelbuild/buildtools" (printf "unused_deps-linux-%s" .chezmoi.arch) }}'
+    ["uv"]='{{ gitHubLatestReleaseAssetURL "astral-sh/uv" (printf "uv-%s-unknown-linux-musl.tar.gz" .sys.uname_arch) }}'
+    ["watchexec"]='{{ gitHubLatestReleaseAssetURL "watchexec/watchexec" (printf "watchexec-*-%s-unknown-linux-musl.deb" .sys.uname_arch) }}'
 
 # {{ if not .is_headless }}
-    ["wezterm"]={{ gitHubLatestReleaseAssetURL "wezterm/wezterm" (printf "wezterm-*.%s%s.deb" .chezmoi.osRelease.name .chezmoi.osRelease.versionID) }}
-    ["firacode"]={{ gitHubLatestReleaseAssetURL "ryanoasis/nerd-fonts" "FiraCode.tar.xz" }}
-    ["firamono"]={{ gitHubLatestReleaseAssetURL "ryanoasis/nerd-fonts" "FiraMono.tar.xz" }}
-    ["inconsolata"]={{ gitHubLatestReleaseAssetURL "ryanoasis/nerd-fonts" "Inconsolata.tar.xz" }}
-    ["victormono"]={{ gitHubLatestReleaseAssetURL "ryanoasis/nerd-fonts" "VictorMono.tar.xz" }}
+    ["wezterm"]='{{ gitHubLatestReleaseAssetURL "wezterm/wezterm" (printf "wezterm-*.%s%s.deb" .chezmoi.osRelease.name .chezmoi.osRelease.versionID) }}'
+    ["firacode"]='{{ gitHubLatestReleaseAssetURL "ryanoasis/nerd-fonts" "FiraCode.tar.xz" }}'
+    ["firamono"]='{{ gitHubLatestReleaseAssetURL "ryanoasis/nerd-fonts" "FiraMono.tar.xz" }}'
+    ["inconsolata"]='{{ gitHubLatestReleaseAssetURL "ryanoasis/nerd-fonts" "Inconsolata.tar.xz" }}'
+    ["victormono"]='{{ gitHubLatestReleaseAssetURL "ryanoasis/nerd-fonts" "VictorMono.tar.xz" }}'
 # {{ end }}
 )
 
