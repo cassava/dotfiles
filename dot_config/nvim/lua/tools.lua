236c13679363ab813f363fc50c2ba99981f3be57
Ben Morgan <cassava@iexu.de>
2025-06-10 15:56:36 +0200

    feat(nvim): Add chezmoi support plugin

--- dot_config/nvim/lua/tools.lua
+++ dot_config/nvim/lua/tools.lua
@@ -54,6 +54,72 @@ return {
     opts = {},
   },
 
+  { "xvzc/chezmoi.nvim",
+    cmd = { "ChezmoiEdit" },
+    keys = {
+      {
+        "<leader>sz",
+        function()
+          local results = require("chezmoi.commands").list({
+            args = {
+              "--path-style",
+              "absolute",
+              "--include",
+              "files",
+              "--exclude",
+              "externals",
+            },
+          })
+          local items = {}
+
+          for _, czFile in ipairs(results) do
+            table.insert(items, {
+              text = czFile,
+              file = czFile,
+            })
+          end
+
+          ---@type snacks.picker.Config
+          local opts = {
+            items = items,
+            confirm = function(picker, item)
+              picker:close()
+              require("chezmoi.commands").edit({
+                targets = { item.text },
+                args = { "--watch" },
+              })
+            end,
+          }
+          Snacks.picker.pick(opts)
+        end,
+        desc = "Chezmoi",
+      },
+    },
+    opts = {
+      edit = {
+        watch = false,
+        force = false,
+      },
+      notification = {
+        on_open = true,
+        on_apply = true,
+        on_watch = false,
+      },
+      telescope = {
+        select = { "<CR>" },
+      },
+    },
+    init = function()
+      -- run chezmoi edit on file enter
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+        callback = function()
+          vim.schedule(require("chezmoi.commands.__edit").watch)
+        end,
+      })
+    end,
+  },
+
   { "folke/snacks.nvim",
     opts = function()
       local Snacks = require("snacks")
