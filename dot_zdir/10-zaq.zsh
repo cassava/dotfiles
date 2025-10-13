12a7db984e53ffe50c8864440a3d7916050365c7
Ben Morgan <cassava@iexu.de>
2025-10-13 11:28:41 +0200

    feat(zsh): Remove zsh-autoquoter module
    I tested it and found that I had to disable it more often than I used it.

--- dot_zdir/10-zaq.zsh
+++
@@ -1,7 +0,0 @@
-ZSH_HIGHLIGHT_HIGHLIGHTERS+=(zaq)
-ZSH_HIGHLIGHT_STYLES[zaq:string]="fg=green,underline"
-ZAQ_PREFIXES=(
-    'git commit( [^ ]##)# -[^ -]#m'
-    'ssh( -[^ ]##)# [^ -][^ ]#'
-)
-ZAQ_PREFIXES_GREEDY=()
