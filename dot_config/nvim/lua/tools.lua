831172cf230a717349895fcf7cbc6f80aed5326e
Ben Morgan <cassava@iexu.de>
2025-10-27 09:25:37 +0100

    fix(nvim): Do not `chezmoi apply` from .git/

--- dot_config/nvim/lua/tools.lua
+++ dot_config/nvim/lua/tools.lua
@@ -205,7 +205,11 @@ return {
       -- run chezmoi edit on file enter
       vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
         pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
-        callback = function()
+        callback = function(ctx)
+          if string.match(ctx.match, "/[.]git/") then
+            -- Ignore unmanaged files
+            return
+          end
           vim.schedule(require("chezmoi.commands.__edit").watch)
         end,
       })
